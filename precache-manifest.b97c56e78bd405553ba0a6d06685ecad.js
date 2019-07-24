08f1b086d34bda7ab4a0ca804f322cda657d3f16
BrandonJoe <brandon.joe42@yahoo.com>
2019-07-24 16:16:54 -0700

    Updates

---
+++ precache-manifest.b97c56e78bd405553ba0a6d06685ecad.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8e5e6b1d4df2cc823fa73bb78a2beafd",
+    "url": "/luxary/index.html"
+  },
+  {
+    "revision": "5fa8492e61c20478cefb",
+    "url": "/luxary/static/css/main.68db0c1f.chunk.css"
+  },
+  {
+    "revision": "6c04564621e539ca1c07",
+    "url": "/luxary/static/js/2.e7505d6b.chunk.js"
+  },
+  {
+    "revision": "5fa8492e61c20478cefb",
+    "url": "/luxary/static/js/main.b6b0ab4a.chunk.js"
+  },
+  {
+    "revision": "3a2d6205b34bfd840cfa",
+    "url": "/luxary/static/js/runtime~main.c78e8fbc.js"
+  },
+  {
+    "revision": "195c77442c431a0138a22914354e7220",
+    "url": "/luxary/static/media/graph1.195c7744.JPG"
+  },
+  {
+    "revision": "3911176bb07a3f0749e443c5becb943d",
+    "url": "/luxary/static/media/graph2.3911176b.JPG"
+  },
+  {
+    "revision": "3abb8eeacbc04a95d1d7fad45515c23f",
+    "url": "/luxary/static/media/profile.3abb8eea.JPG"
+  }
+]);
